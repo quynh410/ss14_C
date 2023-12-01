18609aa18ed3618f51ea763e08471d55e471aaef
quynh410 <147390551+quynh410@users.noreply.github.com>
2023-12-01 09:47:28 +0700

    Add files via upload

---
+++ bai4 ss14.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+ 
+int main()
+{
+	int arr[100];
+	int currentIdex;
+	int size, n;
+	int arryCopy[100];
+	do{
+		printf("**********MENU**********\n");
+		printf("1. Nhap vao mang\n");
+		printf("2. In ra mang\n");
+		printf("3. Sao chep mang vao mang khac\n");
+		printf("4. Nhap vao mang khac, doi cho 2 mang cho nhau\n");
+		printf("5. Dao nguoc mang\n");
+		printf("6. Nhap  vao1 ki tu, tim kiem ki tu do trong mang\n");
+		printf("7. Thoat\n");
+		printf("Lua chon cua ban\n");
+		int choice;
+		scanf("%d", &choice);
+		switch(choice);
+			case 1:
+				
+				break;
+			case 2:
+				
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5 :
+				break;
+			case 6:
+				break:
+			case 7;
+				exit(0);
+				default:
+					printf("Thoat\n");
+				break;
+	}
+}
